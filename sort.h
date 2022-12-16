7a880114a71ff9cda5c62be8ad291f5d145af31d
Oggal <Philip.dan.taylor@gmail.com>
2022-12-16 11:36:09 -0800

    Add Files for Task 2

--- sort.h
+++ sort.h
@@ -27,6 +27,7 @@ void swap_array(int *array, size_t s, int a, int b);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 
 #endif
