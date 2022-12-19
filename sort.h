cf69d6c1c64847f2e9059331c583022ba193ab54
Oggal <Philip.dan.taylor@gmail.com>
2022-12-19 06:06:57 -0800

    Add Files for Task 3

--- sort.h
+++ sort.h
@@ -1,6 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
-
+#include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
 
@@ -28,6 +28,6 @@ void swap_array(int *array, size_t s, int a, int b);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-
+void quick_sort(int *array, size_t size);
 
 #endif
